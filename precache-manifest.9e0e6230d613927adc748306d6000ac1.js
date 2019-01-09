531abed01bbdb947e0588372bfc8a3f7fb26c073
Eugene Kalenik <6361645@gmail.com>
2019-01-09 23:28:24 +0300

    Updates

---
+++ precache-manifest.9e0e6230d613927adc748306d6000ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce530eba077140470e0f",
+    "url": "/i10_calculator_react_redux_typescript/static/js/runtime~main.ce530eba.js"
+  },
+  {
+    "revision": "236d20842d93d918afd3",
+    "url": "/i10_calculator_react_redux_typescript/static/js/main.236d2084.chunk.js"
+  },
+  {
+    "revision": "d2840530200091321f62",
+    "url": "/i10_calculator_react_redux_typescript/static/js/1.d2840530.chunk.js"
+  },
+  {
+    "revision": "236d20842d93d918afd3",
+    "url": "/i10_calculator_react_redux_typescript/static/css/main.92eb4277.chunk.css"
+  },
+  {
+    "revision": "d2840530200091321f62",
+    "url": "/i10_calculator_react_redux_typescript/static/css/1.33412ed8.chunk.css"
+  },
+  {
+    "revision": "8cf3a66982066fa391dc8310f4567bf8",
+    "url": "/i10_calculator_react_redux_typescript/index.html"
+  }
+];
